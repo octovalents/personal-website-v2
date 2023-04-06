6b5a777a9101c180909bca2f1939b41c0caa1b18
benidictusgalihmp <benidictusgalih@gmail.com>
2023-04-06 11:05:56 +0700

    feat: add frontend template

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      <p>This is my personal website.</p>
+      <p>Feel free to look around and learn more about me.</p>
+    </div>
+  );
+};
+
+export default HomePage;
