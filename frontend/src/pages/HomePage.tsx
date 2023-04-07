18fcc119b28f79b56607cfe289cf8f0fecb6c2d1
benidictusgalihmp <benidictusgalih@gmail.com>
2023-04-07 21:08:42 +0700

    feat: add header

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
+import Header from 'components/header/header';
 
 const HomePage: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to My Website</h1>
-      <p>This is my personal website.</p>
-      <p>Feel free to look around and learn more about me.</p>
-    </div>
+    <Header />
   );
 };
 
