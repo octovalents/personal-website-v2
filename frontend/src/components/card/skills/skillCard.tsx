260bea17ad638ea27e93add2f2b17d7ebf555895
benidictusgalihmp <benidictusgalih@gmail.com>
2023-04-16 21:43:54 +0700

    feat: add circle months exp no animation

--- frontend/src/components/card/skills/skillCard.tsx
+++ frontend/src/components/card/skills/skillCard.tsx
@@ -26,35 +26,88 @@ class SkillCard extends React.Component<Props, State> {
 
     render() {
         const { name, monthsExp, tags, lightMode } = this.props;
+        const percentage = 25;
+        const radius = 85;
+        const dashArray = radius * Math.PI * 2;
+        const dashOffset = dashArray - (dashArray * percentage) / 100;
+        const circleWidth = 200;
 
         return (
-            /* Define the structure of your component's UI here */
-            <div
-                className={`oct-card oct-card-skill ${
-                    lightMode ? "light" : ""
-                }`}>
-                {/* screen size > 768px */}
-                <div>
-                    <span>{monthsExp}</span>
-                </div>
-                <div className="oct-container">
-                    <h2
-                        className={`oct-skill-name ${
+            <div>
+                <svg
+                    width={circleWidth}
+                    height={circleWidth}
+                    viewBox={`0 0 ${circleWidth} ${circleWidth}`}>
+                    <circle
+                        cx={circleWidth / 2}
+                        cy={circleWidth / 2}
+                        strokeWidth="10px"
+                        r={radius}
+                        className={`circle-background ${
                             lightMode ? "light" : ""
-                        }`}>
-                        {name}
-                    </h2>
-                    <div className="tag-list">
-                        {tags.map((tag) =>
-                            lightMode ? (
-                                <Tag key={tag} name={tag} lightMode />
-                            ) : (
-                                <Tag key={tag} name={tag} />
-                            )
-                        )}
-                    </div>
-                </div>
+                        }`}
+                    />
+                    <circle
+                        cx={circleWidth / 2}
+                        cy={circleWidth / 2}
+                        strokeWidth="10px"
+                        r={radius}
+                        className={`circle-progress ${
+                            lightMode ? "light" : ""
+                        }`}
+                        style={{
+                            strokeDasharray: dashArray,
+                            strokeDashoffset: dashOffset,
+                        }}
+                        transform={`rotate(-90 ${circleWidth / 2} ${
+                            circleWidth / 2
+                        })`}
+                    />
+                    <text
+                        x="50%"
+                        y="50%"
+                        dy="0.3em"
+                        textAnchor="middle"
+                        className={`circle-text ${lightMode ? "light" : ""}`}>
+                        <tspan
+                            className="months-exp"
+                            x="27%"
+                            y="50%"
+                            dy="0.3em"
+                            textAnchor="middle">
+                            {monthsExp}
+                        </tspan>
+                        <tspan x="59%" y="50%" dy="0.3em" textAnchor="middle">
+                            Months
+                        </tspan>
+                    </text>
+                </svg>
             </div>
+            /* Define the structure of your component's UI here */
+            // <div
+            //     className={`oct-card oct-card-skill ${
+            //         lightMode ? "light" : ""
+            //     }`}>
+            //     {/* screen size > 768px */}
+
+            //     <div className="oct-container">
+            //         <h2
+            //             className={`oct-skill-name ${
+            //                 lightMode ? "light" : ""
+            //             }`}>
+            //             {name}
+            //         </h2>
+            //         <div className="tag-list">
+            //             {tags.map((tag) =>
+            //                 lightMode ? (
+            //                     <Tag key={tag} name={tag} lightMode />
+            //                 ) : (
+            //                     <Tag key={tag} name={tag} />
+            //                 )
+            //             )}
+            //         </div>
+            //     </div>
+            // </div>
         );
     }
 }
