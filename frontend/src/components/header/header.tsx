03d4b9541cecc030b81b7abce1bd42ea20f821ef
benidictusgalihmp <benidictusgalih@gmail.com>
2023-04-18 21:35:15 +0700

    feat: implement basic header

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,21 +1,67 @@
-import React, { useState } from "react";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import React from "react";
 import MyButton from "components/button/button";
+
 import "./header.css";
 
-const Header = () => {
-    return (
-        <header>
-            <MyButton text="Setup" />
-            <MyButton imgSrc="/images/octovalents_400.png" text="Setup" />
-            <MyButton
-                imgSrc="/images/octovalents_400.png"
-                text="Setup"
-                lightMode
-            />
-            <MyButton imgSrc="/images/octovalents_400.png" />
-        </header>
-    );
-};
+interface Props {
+    /* Define the props for your component here */
+    lightMode?: boolean;
+}
 
-export default Header;
+interface State {
+    /* Define the state for your component here */
+    paddings: string;
+}
+
+class MyHeader extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        const scrollPosition = window.scrollY;
+        this.state = {
+            /* Initialize your component's state here */
+            paddings: scrollPosition > 50 ? "20px 40px" : "40px",
+        };
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+
+    handleScroll() {
+        const scrollPosition = window.scrollY;
+        const paddings = scrollPosition > 50 ? "20px 40px" : "40px";
+        this.setState({ paddings });
+    }
+
+    render() {
+        const { lightMode } = this.props;
+        const { paddings } = this.state;
+
+        return (
+            /* Define the structure of your component's UI here */
+            <div
+                className={`oct-header ${lightMode ? "light" : ""}`}
+                style={{ padding: paddings }}>
+                <MyButton imgSrc="svgs/icon.svg" />
+                <div className="topnav menu-container-sm">
+                    <img src="svgs/menu.svg" alt="Menu" />
+                </div>
+                <div className="topnav menu-container-lg">
+                    <MyButton imgSrc="svgs/sun.svg" lightMode={lightMode} />
+                    <MyButton text="Profile" lightMode={lightMode} />
+                    <MyButton text="Experience" lightMode={lightMode} />
+                    <MyButton text="Projects" lightMode={lightMode} />
+                    <MyButton text="Skills" lightMode={lightMode} />
+                    <MyButton text="Contact" lightMode={lightMode} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MyHeader;
