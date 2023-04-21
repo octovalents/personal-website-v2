d4a713747392e4934c9a14365174f0e1a67c388b
octovalents <68523239+octovalents@users.noreply.github.com>
2023-04-21 18:04:35 +0700

    feat: add side navbar

---
+++ frontend/src/components/navbar/sidenavbar/sidenavbar.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import MyButton from "components/button/button";
+
+import "./sidenavbar.css";
+
+interface Props {
+    /* Define the props for your component here */
+    lightMode?: boolean;
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+interface State {
+    /* Define the state for your component here */
+}
+
+class SideNavbar extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            /* Initialize your component's state here */
+        };
+    }
+
+    render() {
+        const { lightMode, isOpen, onClose } = this.props;
+
+        return (
+            /* Define the structure of your component's UI here */
+            <div className="sidenavbar-container">
+                {isOpen && (
+                    <div
+                        className={`sidenavbar-backdrop ${
+                            isOpen ? "open" : "closed"
+                        }`}
+                        onClick={onClose}></div>
+                )}
+                <nav
+                    className={`side-navbar ${isOpen ? "open" : "closed"} ${
+                        lightMode ? "light" : ""
+                    }`}>
+                    <ul>
+                        <li>
+                            <MyButton
+                                imgSrc="svgs/sun.svg"
+                                lightMode={lightMode}
+                            />
+                        </li>
+                        <li>
+                            <MyButton text="Profile" lightMode={lightMode} />
+                        </li>
+                        <li>
+                            <MyButton text="Experience" lightMode={lightMode} />
+                        </li>
+                        <li>
+                            <MyButton text="Projects" lightMode={lightMode} />
+                        </li>
+                        <li>
+                            <MyButton text="Skills" lightMode={lightMode} />
+                        </li>
+                        <li>
+                            <MyButton text="Contact" lightMode={lightMode} />
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default SideNavbar;
