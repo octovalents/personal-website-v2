bf42992e48c67df99424926309a4f5367c6ec54a
octovalents <68523239+octovalents@users.noreply.github.com>
2023-04-25 12:24:02 +0700

    feat: add my profile section

--- frontend/src/components/section/about/about.tsx
+++ frontend/src/components/section/about/about.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import MyButton from "components/button/button";
+
+import "./about.css";
+import "../section.css";
 
 interface Props {
     /* Define the props for your component here */
@@ -22,7 +26,45 @@ class MyAbout extends React.Component<Props, State> {
 
         return (
             /* Define the structure of your component's UI here */
-            <div className={`template ${lightMode ? "light" : ""}`}></div>
+            <div className={`section about ${lightMode ? "light" : ""}`}>
+                <div className="section-title">
+                    <h1
+                        className={`section-title-text ${
+                            lightMode ? "light" : ""
+                        }`}>
+                        My Profile
+                    </h1>
+                    <h5
+                        className={`section-subtitle-text ${
+                            lightMode ? "light" : ""
+                        }`}></h5>
+                </div>
+                <img
+                    className="profile-pic"
+                    src="\svgs\dummy-image.svg"
+                    alt="dummy-image"
+                />
+                <h2 className={`hello ${lightMode ? "light" : ""}`}>Hello!</h2>
+                <p className={`about-desc ${lightMode ? "light" : ""}`}>
+                    My name is Benidictus Galih, a final year undergraduate
+                    student from Institut Teknologi Bandung. I have an
+                    experience learning as a machine learning path student at
+                    Bangkit Academy led by Google, Tokopedia, Gojek, &
+                    Traveloka. Browse my site to know more.
+                </p>
+                <div className="btn-about">
+                    <MyButton
+                        text="Contact me"
+                        imgSrc="\svgs\send.svg"
+                        lightMode={lightMode}
+                    />
+                    <MyButton
+                        text="My resume"
+                        imgSrc="\svgs\file.svg"
+                        lightMode={lightMode}
+                    />
+                </div>
+            </div>
         );
     }
 }
